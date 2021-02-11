78bac74281b43e96b10d7966c927c089bc5d8ee4
Anton <Osipenko>
2021-02-11 16:17:03 +0300

    React start 1.0

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const Test = () => {
+  return <div>hello</div>
+}
+
+
+ReactDOM.render(<Test/>, document.getElementById('root'));
